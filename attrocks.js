b3e82ed4eba664613969ca06f14397b5224870fc
asksnbb <70634908+asksnbb@users.noreply.github.com>
2021-06-04 15:23:20 +0530

    Update attrocks.js
    attrcodksk

--- attrocks.js
+++ attrocks.js
@@ -92,14 +92,14 @@ $(function() {
                       if ($('.google-div').hasClass('d-none')) {
                         $('.google-div').removeClass('d-none');
                         $('.validate_schema').empty();
-                        $('.validate_schema').html("<i class='bx bx-x'></i>");
+                        $('.validate_schema').html("<span>Close<span>");
                         $('.validate_schema').css('background-color','#000');
 
                       }else{
 
                         $('.google-div').addClass('d-none');
                         $('.validate_schema').empty();
-                        $('.validate_schema').html("<i class='bx bxl-google'></i>");
+                        $('.validate_schema').html("<span>Test<span>");
                         $('.validate_schema').css('background-color','green');
                       }
                       
